33673a25e0bfaca16dcdc4fd8e8d900d07efd59b
Erwin Olie <erwin@pop-os.localdomain>
2023-01-06 12:55:05 +0100

    01 - Not Quite Lisp

---
+++ 01 - Not Quite Lisp/Solution.kt
@@ -0,0 +1,17 @@
+package nl.erwinolie.`Advent of Code 2015`.`01 - Not Quite Lisp`
+
+import nl.erwinolie.extensions.input
+
+fun main() {
+    val answer1 = input()
+        .map { if (it == '(') 1 else -1 }
+        .sum()
+    println(answer1)
+
+    val answer2 = input()
+        .map { if (it == '(') 1 else -1 }
+        .runningReduce { acc, i -> acc + i }
+        .indexOfFirst { it == -1 } + 1
+
+    println(answer2)
+}
