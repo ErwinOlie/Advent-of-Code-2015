37d7945e25f92c7d0406a7bff93f1547058ddb58
Erwin Olie <erwin@pop-os.localdomain>
2023-01-06 15:17:22 +0100

    02 - I Was Told There Would Be No Math

--- 02 - I Was Told There Would Be No Math/Solution.kt
+++ 02 - I Was Told There Would Be No Math/Solution.kt
@@ -1,40 +1,19 @@
 package nl.erwinolie.`Advent-of-Code-2015`.`02 - I Was Told There Would Be No Math`
 
 import nl.erwinolie.extensions.input
+import nl.erwinolie.extensions.toRectangle
 
 val boxes = input()
     .lines()
-    .map {
-        val (l, w, h) = "^(\\d+)x(\\d+)x(\\d+)$".toRegex().find(it)!!.destructured
-        Box(l.toInt(), w.toInt(), h.toInt())
-    }
-
-data class Box(
-    val l: Int,
-    val w: Int,
-    val h: Int
-) {
-    fun surfaceArea() =
-        listOf(2*l*w, 2*w*h, 2*h*l).sum()
-
-    fun surfaceAreaSmallestSide() =
-        listOf(l*w, w*h, h*l).min()
-
-    fun shortestDistanceAround() =
-        listOf(2*l, 2*w, 2*h)
-            .sorted().take(2)
-            .sum()
-
-    fun cubicVolume() =
-        l * w * h
-}
+    .map { "^(\\d+)x(\\d+)x(\\d+)$".toRegex().find(it)!! }
+    .map { it.toRectangle() }
 
 fun main() {
     val answer1 = boxes
-        .sumOf { it.surfaceArea() + it.surfaceAreaSmallestSide() }
+        .sumOf { it.area() + it.smallestSideArea() }
     println(answer1)
 
     val answer2 = boxes
-        .sumOf { it.shortestDistanceAround() + it.cubicVolume() }
+        .sumOf { it.shortestDistanceAround() + it.volume() }
     println(answer2)
 }
